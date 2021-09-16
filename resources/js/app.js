2334145105832d0620e4414b4fbd90306b36e57b
JesusArgenis1996 <argenis.sanchez@uabc.edu.mx>
2021-09-15 18:22:10 -0700

    Proyecto todolist

---
+++ resources/js/app.js
@@ -0,0 +1,21 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusSquare, faTrash)
+
+require('./bootstrap');
+
+window.Vue = require('vue').default;
+Vue.component('app', require('./components/app.vue').default);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+});
